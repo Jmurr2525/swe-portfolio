9b0452c7195052a25ed92a591808bd3e13c92cc4
Joe <jtmurray25@gmail.com>
2024-08-11 20:59:47 -0500

    Testing workflow

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -41,7 +41,7 @@ function AboutMe() {
           <motion.p variants={itemVariants} className="text-lg mb-6 text-gray-300">
           I am a young software engineer all about using cool tech to make my life just a little bit easier.
           As a recent Computer Science graduate from the University of Kansas, I've already accumulated a year of hands-on experience as a professional developer. I thrive on versatility,
-          constantly expanding my skill set to become a 'swiss army knife' in the tech industry. 
+          constantly expanding my skill set to become a 'swiss army knife' in the tech industry.  
           </motion.p>
           
           <motion.p variants={itemVariants} className="text-lg mb-6 text-gray-300">
